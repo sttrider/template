94cc42abedf8e39d67cb101d35b65d30a058562b
João Vitor <joao.moreira@hotmart.com>
2019-06-06 07:27:03 -0300

    contrutor fix

--- template/src/main/java/br/com/joao/api/UserAPI.java
+++ template/src/main/java/br/com/joao/api/UserAPI.java
@@ -1,106 +1,99 @@
 package br.com.joao.api;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import br.com.joao.entity.User;
 import br.com.joao.service.UserService;
 import br.com.joao.vo.UserVO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/user")
 @Slf4j
 public class UserAPI {
 
-	@Autowired
-	private UserService userService;
+    private final UserService userService;
 
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
-	@GetMapping("/findAll")
-	public List<UserVO> findAll() {
+    public UserAPI(UserService userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-		return userService.findAll().stream().map(User::toVO).collect(Collectors.toList());
-	}
+    @GetMapping("/findAll")
+    public List<UserVO> findAll() {
 
-	@PostMapping("/findByFilter")
-	public List<UserVO> findAll(@RequestBody UserVO filter) {
+        return userService.findAll().stream().map(User::toVO).collect(Collectors.toList());
+    }
 
-		return userService.findByFilter(filter).stream().map(User::toVO).collect(Collectors.toList());
-	}
+    @PostMapping("/findByFilter")
+    public List<UserVO> findAll(@RequestBody UserVO filter) {
 
-	@PutMapping(path = { "/{id}" }, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Void> update(@PathVariable(name = "id") Long id, @RequestBody UserVO userVO) {
+        return userService.findByFilter(filter).stream().map(User::toVO).collect(Collectors.toList());
+    }
 
-		log.debug("Inicializando o update [{}].", userVO);
+    @PutMapping(path = {"/{id}"}, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> update(@PathVariable(name = "id") Long id, @RequestBody UserVO userVO) {
 
-		Optional<User> userOptional = userService.findById(id);
+        log.debug("Inicializando o update [{}].", userVO);
 
-		if (userOptional.isPresent()) {
-			User user = userOptional.get();
-			user.setName(userVO.getName());
-			user.setEmail(userVO.getEmail());
-			user.setStatus(userVO.getStatus());
-			user.setCreationDateTime(LocalDateTime.now());
-			user.setPassword(passwordEncoder.encode(userVO.getPassword()));
+        Optional<User> userOptional = userService.findById(id);
 
-			return ResponseEntity.noContent().build();
-		} else {
-			log.debug("Entidade não encontrada [{}].", id);
-			return ResponseEntity.notFound().build();
-		}
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setName(userVO.getName());
+            user.setEmail(userVO.getEmail());
+            user.setStatus(userVO.getStatus());
+            user.setCreationDateTime(LocalDateTime.now());
+            user.setPassword(passwordEncoder.encode(userVO.getPassword()));
 
-	}
+            return ResponseEntity.noContent().build();
+        } else {
+            log.debug("Entidade não encontrada [{}].", id);
+            return ResponseEntity.notFound().build();
+        }
 
-	@GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<UserVO> findById(@PathVariable("id") Long id) {
+    }
 
-		log.debug("Buscando entidade [{}].", id);
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserVO> findById(@PathVariable("id") Long id) {
 
-		Optional<User> entity = userService.findById(id);
+        log.debug("Buscando entidade [{}].", id);
 
-		if (!entity.isPresent()) {
-			log.debug("Entidade não encontrada [{}].", id);
-			return ResponseEntity.notFound().build();
-		}
-		try {
-			log.debug("Entidade encontrada [{}].", entity);
-			UserVO vo = entity.get().toVO();
-			log.debug("VO gerado [{}].", vo);
-			return ResponseEntity.ok(vo);
-		} catch (UnsupportedOperationException e) {
-			log.error(e.toString());
-			return ResponseEntity.unprocessableEntity().build();
-		}
-	}
+        Optional<User> entity = userService.findById(id);
 
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> delete(@PathVariable(name = "id") Long id) {
+        if (entity.isEmpty()) {
+            log.debug("Entidade não encontrada [{}].", id);
+            return ResponseEntity.notFound().build();
+        }
+        try {
+            log.debug("Entidade encontrada [{}].", entity);
+            UserVO vo = entity.get().toVO();
+            log.debug("VO gerado [{}].", vo);
+            return ResponseEntity.ok(vo);
+        } catch (UnsupportedOperationException e) {
+            log.error(e.toString());
+            return ResponseEntity.unprocessableEntity().build();
+        }
+    }
 
-		log.debug("Excluindo entidade [{}].", id);
-		if (userService.existsById(id)) {
-			userService.delete(id);
-			return ResponseEntity.ok().build();
-		} else {
-			return ResponseEntity.notFound().build();
-		}
-	}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable(name = "id") Long id) {
+
+        log.debug("Excluindo entidade [{}].", id);
+        if (userService.existsById(id)) {
+            userService.delete(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
